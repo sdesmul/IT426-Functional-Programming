0c5f1f8ee0779d2b14d953fa10b260a70ffde01c
sdesmul <samanthadesmul@yahoo.com>
2020-05-13 00:54:55 -0700

    stream push

--- functional-programming/src/main/java/edu/greenriver/it/functionalprogramming/FunctionalProgrammingController.java
+++ functional-programming/src/main/java/edu/greenriver/it/functionalprogramming/FunctionalProgrammingController.java
@@ -1,18 +1,22 @@
 package edu.greenriver.it.functionalprogramming;
 
-import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Stream;
 
+@Controller
 public class FunctionalProgrammingController {
 
 
     private Map<String, String[]> words = new HashMap<>();
     private Map<String, int[]> numbers = new HashMap<>();
-
 
     public FunctionalProgrammingController() {
 
@@ -20,28 +24,48 @@ public class FunctionalProgrammingController {
 
         words.put("days", new String[]{"Monday", "Tuesday", "Wednesday",
                 "Thursday", "Friday", "Saturday", "Sunday"});
-
-        numbers.put("numbers", new int[10]);
-
     }
 
 
-    @RequestMapping("/problem/1")
+    /**
+     * First problem
+     *
+     * @return
+     */
+    @RequestMapping("/one")
     @ResponseBody
-    public String problem1() {
-        return "";
+    public StringBuilder problem1(Model model) {
+        StringBuilder word = new StringBuilder();
+        String[] favs = words.get("favorite");
+        for (String fav : favs) {
+            word.append(fav).append("\n");
+            System.out.println(word.reverse().toString());
+        }
+        return word;
     }
 
-    @RequestMapping("/problem/2")
+    @RequestMapping("/2")
     @ResponseBody
-    public String problem2() {
-        return "";
+    public Stream<String> problem2(Model model) {
+        String[] dayz = words.get("days");
+        Stream<String> dayzs = Arrays.stream(dayz).filter(s -> {
+            return s.substring(0, 1).chars().count() > 0;
+        });
+
+        model.addAttribute("days", dayz);
+        return Arrays.stream(dayz).distinct();
     }
 
-    @RequestMapping("/problem/3")
+    @RequestMapping("/3")
     @ResponseBody
-    public String problem3() {
-        return "";
+    public int[] problem3() {
+        Random rand = new Random();
+        int[] randomNumbers = new int[10];
+        for (int i = 0; i < randomNumbers.length; i++) {
+            int n = rand.nextInt(100);
+            randomNumbers[i] = n;
+            System.out.println(randomNumbers[i]);
+        }
+        return Arrays.stream(randomNumbers).distinct().toArray();
     }
-
 }
